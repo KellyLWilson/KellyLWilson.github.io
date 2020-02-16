24aa5bc14966787af7f88afd08c976e927c8e1f3
KellyLWilson <60358839+KellyLWilson@users.noreply.github.com>
2020-02-16 14:22:03 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function Choice () {
+    var sun = background color = yellow;
+    var rain = background color = blue;
+    var snow = background color = white;
+
+    if (sun.clicked == true) {
+        box.style.backgroundColor = "yellow";
+    } else if (rain.clicked == true) {
+        box.style.backgroundColor = "blue";
+    } else {
+        snow.style.backgroundColor = "white";
+    };
+};
+
+Choice ();
